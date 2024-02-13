1e074711f69f9c8eb9ead77985dfc4bb225c5e12
Mohammad Abazari <moham.abazari@gmail.com>
2024-02-13 15:43:29 +0330

    original

---
+++ problem0101.m
@@ -0,0 +1,5 @@
+clc;clear;close all;
+clc; clear; close all; format compact;
+addpath('D:\Engineering\Structures\Finite Elements\Finite Element MATLAB\06 Space Truss Element');
+E = 30e6;
+csp = [0,0,0; ]
